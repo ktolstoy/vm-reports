9b62aeeb7fd2fb993fcadb88e02663805a6b6704
Konstantin Tolstoy <konstantin.tolstoy@gmail.com>
2015-05-25 00:14:34 +0300

    Initial commit

---
+++ src/vCenter.java
@@ -0,0 +1,27 @@
+/**
+ * Created by stealth on 5/22/15.
+ */
+
+
+public class vCenter {
+
+    private String hostname;
+    private String username;
+    private String password;
+
+    public static int SetConnectionInfo (String arg_hostname, String arg_username, String arg_password){
+
+//            hostname = arg_hostname;
+//            username = arg_username;
+//            password = arg_password;
+    return 0;
+    }
+
+    public static int Connect (String arg_hostname, String arg_username, String arg_password){
+
+
+
+    return 0;
+    }
+
+}
